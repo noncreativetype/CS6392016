643bdfd8efab187babfdef0cf64cefa12b8bff36
Fabian <frpatino@gmail.com>
2016-07-07 21:00:33 -0400

    firebase project

---
+++ FireBase/app/src/main/java/com/example/fabes/firebase/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.fabes.firebase;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
